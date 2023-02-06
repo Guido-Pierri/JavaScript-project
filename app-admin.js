f701b4e22e79ff556f50bb1f6cc9a8be3262716e
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-02-06 12:41:30 +0100

    admin works

--- app-admin.js
+++ app-admin.js
@@ -10,13 +10,15 @@ const formEl = document.getElementById("form");
 
 const basketlistEl = document.getElementById("basketlist");
 
-const changeItemEl = document.getElementById("changeItem");
+const changeidEl = document.getElementById("changeid");
 const changePriceEl = document.getElementById("changePrice");
-const changeQtyEl = document.getElementById("changeQty");
+const changeproductsEl = document.getElementById("changeproducts");
 const changeCustomerEl = document.getElementById("changeCustomer");
 const changeAdressEl = document.getElementById("changeAdress");
 const changeDeliveryEl = document.getElementById("changeDelivery");
 const changeEmailEl = document.getElementById("changeEmail");
+
+const modifybuttonEL = document.getElementById("modify");
 
 function renderJson(json) {
     let orders = json;
@@ -26,36 +28,48 @@ function renderJson(json) {
     let ordersArray = orders.documents;
     console.log("ordersArray: ", ordersArray);
     for (let i = 0; i < ordersArray.length; i++) {
-        
+
         let orderName = ordersArray[i].name;
         console.log("orderName: ", orderName);
-        
+
         let orderId = orderName.slice(66);
         console.log("orderId: ", orderId);
-        
+
         let orderDate = ordersArray[i].fields.date.timestampValue;
         console.log("orderDate: ", orderDate);
-        
+
         let orderDate1 = new Date(orderDate);
         console.log("orderDate1: ", orderDate1);
-        
+
         let orderShipping = ordersArray[i].fields.shippingMethod.stringValue;
         console.log("orderShipping: ", orderShipping);
-        
+
         let orderPrice = JSON.parse(ordersArray[i].fields.orderTotalPrice.stringValue);
         console.log("orderPrice; ", orderPrice);
         orderPrice = orderPrice.orderTotalPrice;
         console.log("orderPrice; ", orderPrice);
+        let orderProducts = (ordersArray[i].fields.products.stringValue);
+        let tableContent = orderProducts;
 
-        let orderProducts = JSON.parse(ordersArray[i].fields.products.stringValue);
-        console.log("orderProducts", orderProducts);
-        let order = "";
-        for (let j = 0; j < orderProducts.length; j++) {
-            order += `Item id: ${orderProducts[j].id}, ${orderProducts[j].title}, `;
-            // console.log("order", order);
-            
-        }
-            console.log("order", order);
+        // //create unordered list of products
+        // let orderProducts = JSON.parse(ordersArray[i].fields.products.stringValue);
+        // console.log("orderProducts", orderProducts);
+        // var tableContent = '<ul class="list-group list-group-flush">';
+        // for (let index = 0; index < orderProducts.length; index++) {
+        //     tableContent += `<li>ITEM: ${orderProducts[index].title},</li>`;
+        //     // nameCounter++;  // I don't know if this should be there, 
+        //     // logically the counter should be incremented here as well?
+        //     // total.innerHTML = "Total: " + nameCounter;
+        // }
+        // tableContent += '</ul>';
+        // let order = "";
+        // for (let j = 0; j < orderProducts.length; j++) {
+        //     order += `Item nr${j + 1}:Item id: ${orderProducts[j].id}, ${orderProducts[j].title},
+        //      `;
+        //     // console.log("order", order);
+
+        // }
+        //     console.log("order", order);
         let orderAdress = ordersArray[i].fields.adress.stringValue;
         let customerName = ordersArray[i].fields.userName.stringValue;
         let email = ordersArray[i].fields.email.stringValue;
@@ -63,20 +77,25 @@ function renderJson(json) {
         basketlistEl.innerHTML +=
             `
             <tr>
-            <td>${customerName}</td>
             <td>${orderId}</td>
-            <td>${order}</td>
+            <td>${customerName}</td>
+            <td>${orderAdress}</td>
+            <td>${tableContent}</td>
             <td>${orderPrice}</td>
             <td>${email}</td>
-            <td>${orderAdress}</td>
             <td>${orderShipping}</td>
             <td><button onclick='deleteOrder("${orderId}")'>Delete order</button>
-            <td><button onclick='modifyOrder("${orderId}")'; >Modify order</button>
+            <td><button onclick='showForm("${orderId}"), saveId("${orderId}")' >Modify order</button>
 
             <br>
             `
             ;
     }
+}
+
+function saveId(orderId) {
+    console.log("orderId", orderId);
+    localStorage.setItem("ID", orderId);
 }
 function deleteOrder(orderId) {
     alert("deleteOrder");
@@ -94,87 +113,7 @@ function deleteOrder(orderId) {
     setTimeout(reload, 2000);
 };
 
-function modifyOrder(orderId) {
-    showForm();
-    alert("modifyOrder ");
-    let adress = changeAdressEl.value;
-    let price = changePriceEl.value;
-    let quantity = changeQtyEl.value;
-    let frakt = changeDeliveryEl.value;
-    let email = changeEmailEl.value;
-    let itemNumber = changeItemEl.value;
 
-    let url = "https://firestore.googleapis.com/v1/projects/online-oasis-orders/databases/(default)/documents/orders/" + orderId;
-    console.log("url:", url);
-    fetch(url)
-        .then(res => res.json())
-        .then(json => renderJson(json));
-
-    function renderJson(json) {
-        let data = json;
-        console.log("data: ", data);
-        console.log("data.fields: ", data.fields);
-        let userName = data.fields.userName.stringValue;
-        let date = data.fields.date.timestampValue;
-
-
-        if (!quantity || !frakt || !price || !adress || !email || !itemNumber) {
-            alert("You must fill in all parameters to modify an order")
-            return
-        }
-        const body = JSON.stringify({
-            "fields": {
-                "userName": {
-                    "stringValue": userName
-                },
-                "email": {
-                    "stringValue": email
-                },
-                "products": {
-                    "arrayValue": {
-                        "values": [
-                            {
-                                "stringValue": itemNumber
-                            },
-                            {
-                                "stringValue": quantity
-                            }
-                        ]
-                    }
-                },
-                "price": {
-                    "stringValue": price
-                },
-                "date": {
-                    "timestampValue": date
-                },
-                "shippingMethod": {
-                    "stringValue": frakt
-                },
-                "adress": {
-                    "stringValue": adress
-                }
-
-
-            }
-
-
-        });
-
-        url = "https://firestore.googleapis.com/v1/projects/online-oasis-orders/databases/(default)/documents/orders/" + orderId;
-        console.log("url:", url);
-        fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'content-Type': 'application/json'
-            },
-            body: body
-        })
-            .then(res => res.json())
-            .then(data => console.log(data));
-    }
-    // setTimeout(reload,2000)
-}
 
 
 function reload() {
@@ -185,9 +124,89 @@ function finnishOrder() {
     location.assign("http://127.0.0.1:5500/order.html")
 }
 
-function showForm() {
+function showForm(data) {
     console.log("show form");
     formEl.removeAttribute("class")
+    changeidEl.scrollIntoView()
+    let orderId1 = data;
+    console.log("orderId1:", orderId1);
+
+
+    function modifyOrder() {
+       let orderId = localStorage.getItem("ID");
+        console.log("orderID:", orderId);
+        alert("modifyOrder ");
+        let adress = changeAdressEl.value;
+        let price = changePriceEl.value;
+        let frakt = changeDeliveryEl.value;
+        let email = changeEmailEl.value;
+        let idNumber = changeidEl.value;
+        let order = `[{${changeproductsEl.value}}]`;
+        
+        let userName = changeCustomerEl.value;
+
+        let url = "https://firestore.googleapis.com/v1/projects/online-oasis-orders/databases/(default)/documents/orders/" + orderId;
+        console.log("url:", url);
+        fetch(url)
+            .then(res => res.json())
+            .then(json => renderJson(json));
+
+        function renderJson(json) {
+            let data = json;
+            console.log("data: ", data);
+            console.log("data.fields: ", data.fields);
+            let date = data.fields.date.timestampValue;
+
+
+            if (!userName || !frakt || !price || !adress || !email || !idNumber) {
+                alert("You must fill in all parameters to modify an order")
+                return
+            }
+            const body = JSON.stringify({
+                "fields": {
+                    "userName": {
+                        "stringValue": userName
+                    },
+                    "email": {
+                        "stringValue": email
+                    },
+                    "products": {
+                        "stringValue": order
+                    },
+                    "orderTotalPrice": {
+                        "stringValue": price
+                    },
+                    "date": {
+                        "timestampValue": date
+                    },
+                    "shippingMethod": {
+                        "stringValue": frakt
+                    },
+                    "adress": {
+                        "stringValue": adress
+                    }
+
+
+                }
+
+
+            });
+
+            url = "https://firestore.googleapis.com/v1/projects/online-oasis-orders/databases/(default)/documents/orders/" + orderId;
+            console.log("url:", url);
+            fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'content-Type': 'application/json'
+                },
+                body: body
+            })
+                .then(res => res.json())
+                .then(data => console.log(data));
+        }
+        // setTimeout(reload,2000)
+    }
+    modifybuttonEL.addEventListener('click', modifyOrder )
 }
 
 
