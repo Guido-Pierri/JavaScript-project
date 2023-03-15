b0c5fdadd35c054b82eb489cdfc6311822faa4a4
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-03-15 14:33:20 +0100

    add to basket in footer

--- app.js
+++ app.js
@@ -97,23 +97,22 @@ function renderJson(json1) {
         <div class="col-6 col-md-6 col-lg-4 mb-3">
         <div class="card h-100 border border-0 m-3">
         <img src="${productImage}" class="card-img"  alt="${productTitle}" style="height: 300px; width: 100%; object-fit: contain;">
-        <div class="card-body m-0 mb-0">
+        <div class="card-body">
         <div class="card-title">
-        <p class="card-text flex-wrap d-flex justify-content-center" ><b>${productTitle}</b></p>
+        <p class="card-text flex-wrap d-flex justify-content-start"><b>${productTitle}</b></p>
         <div class="d-inline-flex justify-content-between">
         <p>
         <b>${price} kr.</b>
         </p>
-         </div>
-         <div>add to basket
-         <img src="add-to-cart.png" class="d-inline-flex justify-content-between" id='a1' role="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" height="20px"
-         aria-controls="offcanvasWithBothOptions" onclick="addToCart(${productId}); showBasket()"></div>
-
+         </div><br>
+         <a role="button" data-bs-toggle="modal" data-bs-target="#picturemodal${i}">more...</a>
+         
          </div>
          </div>
-         <div class="card-footer">
-      <a role="button" data-bs-toggle="modal" data-bs-target="#picturemodal${i}">more...</a>
-    </div>
+         <div class="card-footer d-flex justify-content-evenly bg-body border-0">
+            <div>add to your basket</div><div class="align-self-center"><img src="add-to-cart.png" class="d-inline-flex justify-content-between" id='a1' role="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" height="20px"
+            aria-controls="offcanvasWithBothOptions" onclick="addToCart(${productId}); showBasket()"></div>
+                </div>
         </div>
         
         </div>
